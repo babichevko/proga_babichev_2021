c2402fb2740d51b1cd1fe1a5282f28ff1270c646
babichevko <babichev.ko@phystech.edu>
2021-10-08 02:45:39 +0300

    оформил функции рисования ног, глаза, рога, волос; исправил ошибки в своём изменении функции дерева: в исходном коде был важен порядок эллипсов, поэтому частично состояние функции пришлось вернуть к изначальному состоянию.

--- refactoring.py
+++ refactoring.py
@@ -20,65 +20,94 @@ hair5 = (175, 233, 221)
 FPS = 30
 screen = pygame.display.set_mode((800, 1200))
 
-
-def tree(x, y, size_x, size_y):
-    rect(screen, white, ((100 - 100) * size_x + x, (630 - 870) * size_y + y, 50 * size_x, 150 * size_y))
-    parametry_ellipsof=[(leaf_green, (20 - 100) * size_x + x, (510 - 870) * size_y + y, 225 * size_x, 175 * size_y, 1), (leaf_green, (15 - 100) * size_x + x, (170 - 870) * size_y + y, 220 * size_x, 240 * size_y,1), (grass_green, (20 - 100) * size_x + x, (510 - 870) * size_y + y, 225 * size_x, 175 * size_y, 3), (grass_green, (15 - 100) * size_x + x, (170 - 870) * size_y + y, 220 * size_x, 240 * size_y, 3), (leaf_green, (-70 - 100) * size_x + x, (340 - 870) * size_y + y, 400 * size_x, 200 * size_y), (grass_green, (-70 - 100) * size_x + x, (340 - 870) * size_y + y, 400 * size_x, 200 * size_y, 3), (fruit_pink, (10 - 100) * size_x + x, (440 - 870) * size_y + y, 30 * size_x, 30 * size_y), (fruit_pink, (170 - 100) * size_x + x, (240 - 870) * size_y + y, 30 * size_x, 30 * size_y, 1), (fruit_pink, (275 - 100) * size_x + x, (455 - 870) * size_y + y, 30 * size_x, 30 * size_y), (fruit_pink, (210 - 100) * size_x + x, (640 - 870) * size_y + y, 30 * size_x, 30 * size_y, 1), (grass_green, (10 - 100) * size_x + x, (440 - 870) * size_y + y, 30 * size_x, 30 * size_y, 3), (grass_green, (170 - 100) * size_x + x, (240 - 870) * size_y + y, 30 * size_x, 30 * size_y, 3), (grass_green, (275 - 100) * size_x + x, (455 - 870) * size_y + y, 30 * size_x, 30 * size_y, 3), (grass_green, (210 - 100) * size_x + x, (640 - 870) * size_y + y, 30 * size_x, 30 * size_y, 3)]
-    for a,b,c,d,e,f in parametry_ellipsof:
-        ellipse(screen, a,b,c,d,e,f)
-
-
-def hair(coord_x, coord_y, size):
-    parametry_ellipsov=[(hair2, coord_x - 45 * size, coord_y + 12 * abs(size), 50 * abs(size), 30 * abs(size)), (hair1, coord_x - 65 * size, coord_y, 65 * abs(size), 20 * abs(size)), (hair1, coord_x - 95 * size, coord_y + 12 * abs(size), 65 * abs(size), 40 * abs(size)), (hair2, coord_x - 75 * size, coord_y + 60 * abs(size), 65 * abs(size), 30 * abs(size)), (hair3, coord_x - 110 * size, coord_y + 40 * abs(size), 90 * abs(size), 30 * abs(size)), (hair4, coord_x - 125 * size, coord_y + 60 * abs(size), 65 * abs(size), 25 * abs(size)), (hair4, coord_x - 72 * size, coord_y + 82 * abs(size), 85 * abs(size), 28 * abs(size)),  (hair3, coord_x - 115 * size, coord_y + 80 * abs(size), 60 * abs(size), 23 * abs(size)), (hair3, coord_x - 100 * size, coord_y + 95 * abs(size), 75 * abs(size), 20 * abs(size)), (hair5, coord_x - 133 * size, coord_y + 90 * abs(size), 45 * abs(size), 15 * abs(size)), (hair5, coord_x - 133 * size, coord_y + 100 * abs(size), 70 * abs(size), 18 * abs(size)), (hair3, coord_x - 95 * size, coord_y + 105 * abs(size), 70 * abs(size), 20 * abs(size)), (hair5, coord_x - 65 * size, coord_y + 105 * abs(size), 62 * abs(size), 23 * abs(size)), (pink, coord_x - 160 * size, coord_y + 113 * abs(size), 90 * abs(size), 22 * abs(size))]
-    for a,b,c,d,e in parametry_ellipsov:
-        ellipse(screen, a, (b, c, d, e))
-
-
-
-def unicorn(x, y, size):
-    hair((190 - 645) * size + x, (860 - 655) * size + y, - size)
-    hair((390 - 645) * size + x, (810 - 655) * size + y, size)
-    ellipse(screen, white, ((375 - 645) * size + x, (750 - 655) * size + y, 310 * size, 130 * size))
-    ellipse(screen, white, ((565 - 645) * size + x, (620 - 655) * size + y, 110 * size, 75 * size))
-    ellipse(screen, white, ((620 - 645) * size + x, (645 - 655) * size + y, 100 * size, 50 * size))
-    rect(screen, white, ((565 - 645) * size + x, (670 - 655) * size + y, 90 * size, 130 * size))
-    rect(screen, white, ((395 - 645) * size + x, (845 - 655) * size + y, 20 * size, 155 * size))
-    rect(screen, white, ((465 - 645) * size + x, (860 - 655) * size + y, 25 * size, 130 * size))
-    rect(screen, white, ((565 - 645) * size + x, (870 - 655) * size + y, 22 * size, 150 * size))
-    rect(screen, white, ((625 - 645) * size + x, (850 - 655) * size + y, 18 * size, 140 * size))
-    circle(screen, pink, ((640 - 645) * size + x, (655 - 655) * size + y), 13 * size)
-    circle(screen, black, ((645 - 645) * size + x, (655 - 655) * size + y), 5 * size)
-    ellipse(screen, white, ((633 - 645) * size + x, (650 - 655) * size + y, 10 * size, 3 * size))
-    hair((604 - 645) * size + x, (630 - 655) * size + y, size)
-    polygon(screen, pink,
-            [((606 - 645) * size + x, (621 - 655) * size + y), ((633 - 645) * size + x, (500 - 655) * size + y),
-             ((635 - 645) * size + x, (625 - 655) * size + y)])
-    ellipse(screen, hair1, ((575 - 645) * size + x, (610 - 655) * size + y, 50 * size, 30 * size))
-
+#Здесь описаны все функции рисования
 
 def sun(x_center, y_center, radius):
     circle(screen, yellow, (x_center, y_center), radius)
 def grass(y, dlina, shirina):
     rect(screen, grass_green, (0, y, dlina, shirina))
-def sky(dlina,shirina):
-        rect(screen, sky_blue, dlina, shirina)
+def sky(dlina, shirina):
+        rect(screen, sky_blue, (0, 0, dlina, shirina))
+
+
+def tree(x, y, size_x, size_y):
+    rect(screen, white, ((100 - 100) * size_x + x, (630 - 870) * size_y + y, 50 * size_x, 150 * size_y))
+
+    ellipse(screen, leaf_green, ((20 - 100) * size_x + x, (510 - 870) * size_y + y, 225 * size_x, 175 * size_y))
+    ellipse(screen, leaf_green, ((15 - 100) * size_x + x, (170 - 870) * size_y + y, 220 * size_x, 240 * size_y))
+    ellipse(screen, grass_green, ((20 - 100) * size_x + x, (510 - 870) * size_y + y, 225 * size_x, 175 * size_y), 3)
+    ellipse(screen, grass_green, ((15 - 100) * size_x + x, (170 - 870) * size_y + y, 220 * size_x, 240 * size_y), 3)
+    ellipse(screen, leaf_green, ((-70 - 100) * size_x + x, (340 - 870) * size_y + y, 400 * size_x, 200 * size_y))
+    ellipse(screen, grass_green, ((-70 - 100) * size_x + x, (340 - 870) * size_y + y, 400 * size_x, 200 * size_y), 3)
+
+    ellipsy2 = [
+        (fruit_pink, (10 - 100) * size_x + x, (440 - 870) * size_y + y, 30 * size_x, 30 * size_y),
+        (fruit_pink, (170 - 100) * size_x + x, (240 - 870) * size_y + y, 30 * size_x, 30 * size_y),
+        (fruit_pink, (275 - 100) * size_x + x, (455 - 870) * size_y + y, 30 * size_x, 30 * size_y),
+        (fruit_pink, (210 - 100) * size_x + x, (640 - 870) * size_y + y, 30 * size_x, 30 * size_y)]
+
+    for a, b, c, d, e, in ellipsy2:
+        ellipse(screen, a, (b, c, d, e))
+
+    ellipsy3 = [((10 - 100) * size_x + x, (440 - 870) * size_y + y, 30 * size_x, 30 * size_y), ((170 - 100) * size_x + x, (240 - 870) * size_y + y, 30 * size_x, 30 * size_y), ((275 - 100) * size_x + x, (455 - 870) * size_y + y, 30 * size_x, 30 * size_y), ((210 - 100) * size_x + x, (640 - 870) * size_y + y, 30 * size_x, 30 * size_y)]
+
+    for b, c, d, e in ellipsy3:
+        ellipse(screen, grass_green, (b, c, d, e), 3)
+
+def pricheska_edinoroga(coord_x, coord_y, size):
+    parametry_ellipsov=[(hair2, coord_x - 45 * size, coord_y + 12 * abs(size), 50 * abs(size), 30 * abs(size)), (hair1, coord_x - 65 * size, coord_y, 65 * abs(size), 20 * abs(size)), (hair1, coord_x - 95 * size, coord_y + 12 * abs(size), 65 * abs(size), 40 * abs(size)), (hair2, coord_x - 75 * size, coord_y + 60 * abs(size), 65 * abs(size), 30 * abs(size)), (hair3, coord_x - 110 * size, coord_y + 40 * abs(size), 90 * abs(size), 30 * abs(size)), (hair4, coord_x - 125 * size, coord_y + 60 * abs(size), 65 * abs(size), 25 * abs(size)), (hair4, coord_x - 72 * size, coord_y + 82 * abs(size), 85 * abs(size), 28 * abs(size)),  (hair3, coord_x - 115 * size, coord_y + 80 * abs(size), 60 * abs(size), 23 * abs(size)), (hair3, coord_x - 100 * size, coord_y + 95 * abs(size), 75 * abs(size), 20 * abs(size)), (hair5, coord_x - 133 * size, coord_y + 90 * abs(size), 45 * abs(size), 15 * abs(size)), (hair5, coord_x - 133 * size, coord_y + 100 * abs(size), 70 * abs(size), 18 * abs(size)), (hair3, coord_x - 95 * size, coord_y + 105 * abs(size), 70 * abs(size), 20 * abs(size)), (hair5, coord_x - 65 * size, coord_y + 105 * abs(size), 62 * abs(size), 23 * abs(size)), (pink, coord_x - 160 * size, coord_y + 113 * abs(size), 90 * abs(size), 22 * abs(size))]
+    for a,b,c,d,e in parametry_ellipsov:
+        ellipse(screen, a, (b, c, d, e))
+def nogi_edinoroga(x, y, size):
+    parametry_nog=[((565 - 645) * size + x, (670 - 655) * size + y, 90 * size, 130 * size), ((395 - 645) * size + x, (845 - 655) * size + y, 20 * size, 155 * size), ((465 - 645) * size + x, (860 - 655) * size + y, 25 * size, 130 * size), ((565 - 645) * size + x, (870 - 655) * size + y, 22 * size, 150 * size), ((625 - 645) * size + x, (850 - 655) * size + y, 18 * size, 140 * size)]
+    for a,b,c,d in parametry_nog:
+        rect(screen, white, (a, b, c, d))
+def glaz_edinoroga(x, y, size):
+    circle(screen, pink, ((640 - 645) * size + x, (655 - 655) * size + y), 13 * size)
+    circle(screen, black, ((645 - 645) * size + x, (655 - 655) * size + y), 5 * size)
+def rog_edinoroga(x,y,size):
+    polygon(screen, pink,
+            [((606 - 645) * size + x, (621 - 655) * size + y), ((633 - 645) * size + x, (500 - 655) * size + y),
+             ((635 - 645) * size + x, (625 - 655) * size + y)])
+
+def telo_edinoroga(x, y, size):
+    parametry_ellipsov=[(white, (375 - 645) * size + x, (750 - 655) * size + y, 310 * size, 130 * size), (white, (565 - 645) * size + x, (620 - 655) * size + y, 110 * size, 75 * size), (white, (620 - 645) * size + x, (645 - 655) * size + y, 100 * size, 50 * size), (hair1, (575 - 645) * size + x, (610 - 655) * size + y, 50 * size, 30 * size), (white, (633 - 645) * size + x, (650 - 655) * size + y, 10 * size, 3 * size)]
+    for a,b,c,d,e in parametry_ellipsov:
+        ellipse(screen, a, (b,c,d,e))
+    nogi_edinoroga(x, y, size)
+    glaz_edinoroga(x, y, size)
+    rog_edinoroga(x, y, size)
+
+def edinorog(x, y, size):
+    telo_edinoroga(x,y,size)
+    volosy = [((190 - 645) * size + x, (860 - 655) * size + y, - size),
+              ((390 - 645) * size + x, (810 - 655) * size + y, size),
+              ((604 - 645) * size + x, (630 - 655) * size + y, size)]
+    for A, B, C in volosy:
+        pricheska_edinoroga(A, B, C)
+
+
+
+
+
 
 #Здесь рисуем всё что нужно
+
 sky(800, 520)
-grass(520, 800, 1200)
+grass (520, 800, 1200)
 sun (770, 120, 150)
 
 for i in range(150):
     circle(screen, (255, 255, 155 - i), (770, 120), 150 - i)
 
 
-trees=[(220, 600, 1, 1), (350, 640, 0.7, 0.3), (165, 870, 1, 0.7), (40, 750, 0.7, 0.7), (70, 1100, 0.7, 0.7)]
-unicorns=[(400, 800, 0.9), (675, 655, 0.5), (550, 450, 0.6), (750, 480, 0.3), (710, 900, 0.6)]
+trees = [(220, 600, 1, 1), (350, 640, 0.7, 0.3), (165, 870, 1, 0.7), (40, 750, 0.7, 0.7), (70, 1100, 0.7, 0.7)]
+edinorogi = [(400, 800, 0.9), (675, 655, 0.5), (550, 450, 0.6), (750, 480, 0.3), (710, 900, 0.6)]
 
 for a,b,c,d in trees:
     tree(a, b, c, d)
-for a, b, c in unicorns:
-    unicorn(a, b, c)
+for a, b, c in edinorogi:
+    edinorog(a, b, c)
 
 
 
