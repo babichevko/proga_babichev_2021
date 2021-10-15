39ff68bb067f12eb6c68fd7ccaef6cae7ba27258
babichevko <babichev.ko@phystech.edu>
2021-10-15 11:11:48 +0300

    Добавлена новая фигура - квадрат, за неё начисляется больше очков; также добавлено уменьшение очков в случае бездействия с течением времени

--- поймайшарик.py
+++ поймайшарик.py
@@ -17,13 +17,13 @@ COLORS = [RED, BLUE, YELLOW, GREEN, MAGENTA, CYAN]
 
 global chislo_ochkov
 chislo_ochkov = 0
-myfont = pygame.font.SysFont('Comic Sans MS', 30)
-textsurface = myfont.render('Ваши очки:', False, (0, 0, 0))
 
 
 "Массивы для параметров шаров и их скоростей"
 balls = []
+polygons=[]
 v=[]
+v2=[]
 
 "Функция создания параметров нового шара"
 def new_ball():
@@ -43,6 +43,38 @@ def ball_speed():
     vy = randint(-10, 10)
     v.append([vx, vy])
 
+"Функция создания параметров нового квадрата"
+def new_polygon():
+    x2 = randint(100, 700)
+    y2 = randint(100, 500)
+    r2 = randint(30, 70)
+    width = randint(3, 10)
+    color2 = COLORS[randint(0, 5)]
+    polygons. append([color2, x2, y2, r2, width])
+
+"Функция присваивания скорости новому квадрату"
+def polygon_speed():
+    vx2 = randint(-10, 10)
+    vy2 = randint(-10, 10)
+    v2.append([vx2, vy2])
+
+"Функция отрисовки нового шара"
+def draw_polygon(color, x2, y2, r2, width):
+    polygon(screen, color, [(x2, y2), (x2 + r2, y2), (x2 + r2, y2 + r2), (x2, y2 + r2)], width)
+
+"Функция проверки попадания игрока по квадрату"
+def proverka_najatia2(coordinaty_najatiya, coordinata_kvadrata_x, coordinata_kvadrata_y, shirina_kvadrata):
+    global najal2
+    najal2 = False
+    coordinata_najatiya_x = coordinaty_najatiya[0]
+    coordinata_najatiya_y = coordinaty_najatiya[1]
+
+    # проверка попадания в квадрат
+    raznost1 = (coordinata_najatiya_x - coordinata_kvadrata_x)
+    raznost2  = (coordinata_najatiya_y - coordinata_kvadrata_y)
+    if raznost1 > 0 and raznost2 > 0 and raznost1 < shirina_kvadrata and raznost2 < shirina_kvadrata:
+        najal2 = True
+
 "Функция проверки попадания игрока по шару"
 def proverka_najatia(coordinaty_najatiya, coordinata_kruga_x, coordinata_kruga_y, radius_kruga):
     global najal
@@ -60,7 +92,7 @@ def proverka_najatia(coordinaty_najatiya, coordinata_kruga_x, coordinata_kruga_y
 def schet():
     schetchik = pygame.font.Font(None, 100)
     textsurface = schetchik.render(str(chislo_ochkov), False, RED)
-    screen.blit(textsurface,(100, 100))
+    screen.blit(textsurface, (100, 100))
 
 def zavershenie_igry():
     clock.tick(FPS)
@@ -75,19 +107,27 @@ def zavershenie_igry():
     screen.blit(textsurface3, (500, 600))
     pygame.display.update()
 
-"Создаём изначально несколько шаров"
-for i in range(5, 10):
-    new_ball()
-for i in range(len(balls)):
-    ball_speed()
+def nachalo_igry():
+    "Создаём изначально несколько шаров  и квадратов"
+    for i in range(5, 10):
+        new_ball()
+    for i in range(len(balls)):
+        ball_speed()
+    for i in range(5, 10):
+        new_polygon()
+    for i in range(len(polygons)):
+        polygon_speed()
 
 pygame.display.update()
 
 clock = pygame.time.Clock()
 finished = False
 
+nachalo_igry()
+
 while not finished:
     g = 0
+    h = 0
     clock.tick(FPS)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -97,7 +137,7 @@ while not finished:
                 for i in range(len(balls)):
                     proverka_najatia(event.pos, balls[i][1], balls[i][2], balls[i][3])
                     "Если попали по шару - удаляем его, делаем новый, счёт увеличивается"
-                    if najal == True:
+                    if najal is True:
                         new_ball()
                         #Заменяем старый шар на новый
                         balls[i] = balls[len(balls)-1]
@@ -106,31 +146,59 @@ while not finished:
                         ball_speed()
                         v[i] = v[len(v) - 1]
                         v.pop(len(v) - 1)
-                        chislo_ochkov += 1
+                        chislo_ochkov += 100
                         g = 1
-            #Если не попали - число шаров увеличивается
+                for i in range(len(polygons)):
+                    proverka_najatia2(event.pos, polygons[i][1], polygons[i][2], polygons[i][3])
+                    "Если попали по квадрату - удаляем его, делаем новый, счёт увеличивается"
+                    if najal2 is True:
+                        new_polygon()
+                        # Заменяем старый квадрат на новый
+                        polygons[i] = polygons[len(polygons) - 1]
+                        polygons.pop(len(polygons) - 1)
+                        # Заменяем скорость старого шара на скорость нового
+                        polygon_speed()
+                        v2[i] = v2[len(v2) - 1]
+                        v2.pop(len(v2) - 1)
+                        chislo_ochkov += 200
+                        h = 1
+            #Если не попали - число шаров и квадратов увеличивается
             if g != 1:
                 new_ball()
                 ball_speed()
-                g=0
 
-    #И когда шаров слишком много - игра заканчивается
-    if len(balls) > 20:
+            if h != 1 and g != 1:
+                new_polygon()
+                polygon_speed()
+
+
+    #И когда шаров и квадратов слишком много - игра заканчивается
+    if len(balls) + len(polygons) > 40:
         finished = True
 
+    #Если ничего не делать, число очков постепенно уменьшается
+    chislo_ochkov -= 1
     schet()
 
-    #Рисуем шары
+    #Рисуем шары и квадраты
     for i in range(len(balls)):
-            color, x, y, r = balls[i]
-            draw_ball(color, x, y, r)
+        color, x, y, r = balls[i]
+        draw_ball(color, x, y, r)
 
-    "Изменение положения шаров"
+    for i in range(len(polygons)):
+        color, x2, y2, r2, width = polygons[i]
+        draw_polygon(color, x2, y2, r2, width)
+
+    "Изменение положения шаров и квадратов"
     for i in range(len(balls)):
         balls[i][1] += v[i][0]
         balls[i][2] += v[i][1]
+    for i in range(len(polygons)):
+        polygons[i][1] += v2[i][0]
+        polygons[i][2] += v2[i][1]
 
         "Отражение шаров от стенок"
+    for i in range(len(balls)):
         if balls[i][1] - 2 * balls[i][3] < 0 and v[i][0] < 0 or balls[i][1] + 2 * balls[i][3] > 1200 and v[i][0] > 0:
             k = randint(5, 18)
             v[i][0] = -k * v[i][0] / 10
@@ -139,6 +207,7 @@ while not finished:
                 v[i][0] = -5
             if v[i][0] > 30:
                 v[i][0] = 5
+
         if balls[i][2] - 2 * balls[i][3] < 0 and v[i][1] < 0 or balls[i][2] + 2 * balls[i][3] > 1200 and v[i][1] > 0:
             t = randint(5, 18)
             v[i][1] = -t * v[i][1] / 10
@@ -149,16 +218,32 @@ while not finished:
             if v[i][1] > 30:
                 v[i][1] = -5
 
+    "Отражение квадратов от стенок"
+    for i in range(len(polygons)):
+        if polygons[i][1] < 0 and v2[i][0] < 0 or polygons[i][1] + polygons[i][3] > 1200 and v2[i][0] > 0:
+            k = randint(5, 18)
+            v2[i][0] = -k * v[i][0] / 10
+            "Ограничим скорость квадратов"
+            if v2[i][0] < -50:
+                v2[i][0] = 10
+            if v2[i][0] > 50:
+                v2[i][0] = -10
+
+        if polygons[i][2] < 0 and v2[i][1] < 0 or polygons[i][2] + polygons[i][3] > 1200 and v2[i][1] > 0:
+            t = randint(5, 18)
+            v2[i][1] = -t * v2[i][1] / 10
+
+            "Ограничим скорость квадратов"
+            if v2[i][1] < -50:
+                v2[i][1] = 10
+            if v2[i][1] > 50:
+                v2[i][1] = -10
+
     pygame.display.update()
+
     screen.fill(BLACK)
 
-for i in range(150):
+for i in range(100):
     zavershenie_igry()
 
 pygame.quit()
-
-
-
-
-
-
